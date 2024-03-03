5b3eea0e3c133e8ef2142a407eb2693f81a42982
iOS 开发工程师 <1002230810@qq.com>
2024-03-03 16:10:10 +0800

    first commit

---
+++ TestHotRoload/TestHotRoload/QWRStateTitleView.h
@@ -0,0 +1,16 @@
+//
+//  QWRStateTitleView.h
+//  TestHotRoload
+//
+//  Created by 何鹏 on 2024/2/28.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QWRStateTitleView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestHotRoload/TestHotRoload/QWRStateTitleView.m
@@ -0,0 +1,20 @@
+//
+//  QWRStateTitleView.m
+//  TestHotRoload
+//
+//  Created by 何鹏 on 2024/2/28.
+//
+
+#import "QWRStateTitleView.h"
+
+@implementation QWRStateTitleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
